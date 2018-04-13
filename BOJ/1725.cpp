7046e696d9aed7ea724526c732cb059b5cb64056
KyungJun <parkryan91@gmail.com>
2018-04-14 00:52:58 +0900

    BOJ 1725 commit

--- BOJ/1725.cpp
+++ BOJ/1725.cpp
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <algorithm>
+#include <stack>
+#define MAX_N 100000
+using namespace std;
+
+FILE *fin = stdin;
+FILE *fout = stdout;
+
+int N;
+int Arr[MAX_N + 1];
+stack<int> st;
+int Ans;
 
 int main()
 {
-    printf("비와이");
+    int i;
+
+    fscanf(fin,"%d",&N);
+    for(i=1;i<=N;i++) fscanf(fin,"%d",&Arr[i]);
+
+    for(i=1;i<=N;i++) {
+        while(!st.empty() && Arr[i] < Arr[st.top()]) {
+            int pos = st.top();
+            st.pop();
+            int width = !st.empty() ? i - st.top() - 1 : i - 1;
+            int area = Arr[pos] * width;
+            Ans = Ans < area ? area : Ans;
+        }
+        st.push(i);
+    }
+    while(!st.empty()) {
+        int pos = st.top();
+        st.pop();
+        int width = !st.empty() ? N - st.top() : N;
+        int area = Arr[pos] * width;
+        Ans = Ans < area ? area : Ans;
+    }
+    fprintf(fout,"%d",Ans);
     return 0;
 }
