ef203555548f92ae93ceb90fec31b27213e08a0c
KyungJun <parkryan91@gmail.com>
2018-04-21 23:35:53 +0900

    move the file

---
+++ BOJ/1475.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <algorithm>
+#include <memory.h>
+#include <vector>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#define MAX 400005
+
+using namespace std;
+
+FILE *fin = stdin;
+FILE *fout = stdout;
+
+int N;
+int Arr[10];
+int Ans;
+
+void Input()
+{
+	fscanf(fin, "%d", &N);
+}
+
+void Process()
+{
+	int i;
+    if (N==0) Arr[0]++;
+	while (N > 0) {
+		Arr[N % 10]++;
+		N /= 10;
+	}
+	for (i = 0; i <= 9; i++) {
+		int num = Arr[i];
+		if (i == 6 || i == 9) {
+            num = Arr[6] + Arr[9];
+            if (num%2==1) num = num/2 +1;
+            else num /= 2;
+		}
+		Ans = std::max(Ans, num);
+	}
+}
+
+void Output()
+{
+	fprintf(fout, "%d", Ans);
+}
+
+int main()
+{
+	int t, p;
+
+	//    fscanf(fin,"%d",&t);
+	t = 1;
+	for (p = 1; p <= t; p++) {
+		Input();
+		Process();
+		Output();
+	}
+	return 0;
+}
